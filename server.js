e737524f8b076342ef83f130be48fa28a4d94f53
Thomas <tbeckman1234@gmail.com>
2023-11-08 11:20:12 -0700

    Enabled CORS

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ const app = express()
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://mern-frontend-umber.vercel.app/")
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    )
+    res.header("Access-Control-Allow-Origin", "https://mern-frontend-umber.vercel.app/")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     console.log(req.path, req.method)
     next()
 })
