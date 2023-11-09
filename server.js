f5085ac8ac0b5a99cad19bfcd41dcbf1e41e072b
Thomas <tbeckman1234@gmail.com>
2023-11-09 11:08:18 -0700

    added Authorization to allowed headers

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://mern-frontend-umber.vercel.app")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS")
     console.log(req.path, req.method)
     next()
