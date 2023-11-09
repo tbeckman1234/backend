8b5cce936c84712d49a6398a5d97e2369b047cc9
Thomas <tbeckman1234@gmail.com>
2023-11-09 16:25:27 -0700

    added allowed methods to CORS

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://mern-frontend-umber.vercel.app")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     res.header("Access-Control-Allow-Credentials", "true")
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
     console.log(req.path, req.method)
     next()
 })
