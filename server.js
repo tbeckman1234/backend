4e1c3366eee8d8208b800ae3720c31a00408f5bf
Thomas <tbeckman1234@gmail.com>
2023-11-09 16:27:19 -0700

    Added Vary Accept-Endcoding

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     res.header("Access-Control-Allow-Credentials", "true")
     res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+    res.header("Vary", "Accept-Encoding, Origin")
     console.log(req.path, req.method)
     next()
 })
