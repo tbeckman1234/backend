7e7dbfdccf8beeb8b0edbbdba605c61a4b84af12
Thomas <tbeckman1234@gmail.com>
2023-11-13 15:52:40 -0700

    added wildcard * in allowed origin cors policy

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const app = express()
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://mern-frontend-umber.vercel.app")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization")
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     res.header("Access-Control-Allow-Credentials", "true")
     res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
     res.header("Vary", "Accept-Encoding, Origin")
