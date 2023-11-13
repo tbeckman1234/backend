4575fb3a01ff2dee848c3a34a69778dd94d97411
Thomas <tbeckman1234@gmail.com>
2023-11-13 15:54:44 -0700

    added https://mern-backend-tau.vercel.app/api/

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Origin", "https://mern-backend-tau.vercel.app/api/")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     res.header("Access-Control-Allow-Credentials", "true")
     res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
