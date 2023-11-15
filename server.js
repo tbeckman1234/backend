c0d241d79c94e5d9fa114f62013181d9465e29e0
Thomas <tbeckman1234@gmail.com>
2023-11-15 12:12:21 -0700

    made it complicated

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
+const corsWhiteList = ['https://mern-frontend-umber.vercel.app/api/workouts','https://mern-frontend-umber.vercel.app/']
+const corsOptions = (req, callback) => {
+    if (corsWhiteList.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true }
+    } else {
+        corsOptions = { origin: false }
+    }
+    callback(null, corsOptions)
+}
 
 // initialize express app
 const app = express()
@@ -13,9 +22,7 @@ const app = express()
 app.use(express.json())
 
 // enable CORS
-app.use(cors({
-    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
-}))
+app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
@@ -23,7 +30,7 @@ app.use((req, res, next) => {
 })
 
 // route handler
-app.use('/api/workouts', cors(), workoutRoutes)
+app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
 
 // connect to db
